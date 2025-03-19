72280b1767fdc68cb507ec79099049001e1d5fe9
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-03-19 12:10:14 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DataAccessAuthDemo from './components/DataAccessAuthDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <DataAccessAuthDemo />
+    </div>
+  );
+}
+
+export default App;
